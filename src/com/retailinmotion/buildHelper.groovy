d879ff94f5226e216c86995ff391b9d2e8f182f0
Keith Douglas <keith.douglas@retailinmotion.com>
2018-03-13 08:57:30 +0000

    Modified getChangeString function

--- src/com/retailinmotion/buildHelper.groovy
+++ src/com/retailinmotion/buildHelper.groovy
@@ -244,13 +244,8 @@ def getChangeString() {
 		for (int j = 0; j < entries.length; j++) {
 			def entry = entries[j]
 			echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-			changeString+=entry.msg
+			changeString+=entry.msg + "\n"
 			
-			def files = new ArrayList(entry.affectedFiles)
-			for (int k = 0; k < files.size(); k++) {
-				def file = files[k]
-				echo "  ${file.editType.name} ${file.path}"
-			}
 		}
 	}
 	
