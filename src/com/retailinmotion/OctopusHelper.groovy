9b672a2f997278595839eb0b07cfe8e928e13c7c
Keith Douglas <keith.douglas@retailinmotion.com>
2018-11-29 10:58:43 +0000

    - Fixed missing octopusserver config in OctopusHelper.execOcto

--- src/com/retailinmotion/OctopusHelper.groovy
+++ src/com/retailinmotion/OctopusHelper.groovy
@@ -44,7 +44,7 @@ def getServer(jenkinsURL){
 	return octopus
 }
 
-def execOcto(commandOptions){
+def execOcto(octopusServer, commandOptions){
 	if(isUnix()){
 		sh "docker run --rm -v \$(pwd):/src octopusdeploy/octo ${commandOptions}"
 	} else {
@@ -62,7 +62,7 @@ def listDeployments (jenkinsURL, tenant, environment){
 	def octopusServer=getServer(jenkinsURL)
 	withCredentials([string(credentialsId: octopusServer.credentialsId, variable: 'APIKey')]) {			
 		def commandOptions="list-deployments --tenant=${tenant} --environment=${environment} --server ${octopusServer.url} --apiKey ${APIKey}"
-		execOcto(commandOptions)
+		execOcto(octopusServer, commandOptions)
 	}
 }
 
@@ -74,7 +74,7 @@ def pushPackage (jenkinsURL, packageFile){
 	println "Pushing package $packageFile to ${octopusServer.url}"
 	withCredentials([string(credentialsId: octopusServer.credentialsId, variable: 'APIKey')]) {			
 		def commandOptions="push --package $packageFile --server ${octopusServer.url} --apiKey ${APIKey}"
-		execOcto(commandOptions)
+		execOcto(octopusServer, commandOptions)
 	}
 }
 
@@ -87,7 +87,7 @@ def deploy(jenkinsURL, project, packageString, deployTo, extraArgs){
 	withCredentials([string(credentialsId: octopusServer.credentialsId, variable: 'APIKey')]) {			
 		def commandOptions=" --create-release --waitfordeployment --progress --project \"$project\" --packageversion $packageString --version $packageString --deployTo \"$deployTo\" $extraArgs --server ${octopusServer.url} --apiKey ${APIKey}"
 		
-		execOcto(commandOptions)
+		execOcto(octopusServer, commandOptions)
 	}
 }
 
@@ -110,7 +110,7 @@ def createRelease(jenkinsURL, project, releaseVersion, packageArg = "", channel=
 	
 	withCredentials([string(credentialsId: octopusServer.credentialsId, variable: 'APIKey')]) {			
 		def commandOptions="--create-release --project \"$project\" $optionString --force --version $releaseVersion $extraArgs --server ${octopusServer.url} --apiKey ${APIKey}"
-		execOcto(commandOptions)
+		execOcto(octopusServer, commandOptions)
 	}
 }
 
@@ -123,7 +123,7 @@ def createReleaseFromFolder(jenkinsURL, project, releaseVersion, packagesFolder,
 	def octopusServer=getServer(jenkinsURL)
 	withCredentials([string(credentialsId: octopusServer.credentialsId, variable: 'APIKey')]) {		
 		def commandOptions="--create-release --project \"$project\" --packagesFolder \"$packagesFolder\" --version $releaseVersion $extraArgs --server ${octopusServer.url} --apiKey ${APIKey}"
-		execOcto(commandOptions)
+		execOcto(octopusServer, commandOptions)
 	}
 }
 
