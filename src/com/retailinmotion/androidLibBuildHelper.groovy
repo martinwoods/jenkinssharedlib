f8fd9556c37c2031da3a03c660ce536367a96ca8
Vlad Petrescu <vlad.petrescu@retailinmotion.com>
2020-01-27 15:54:37 +0000

    DEVOPS-316: added checkout and gitversion steps

--- src/com/retailinmotion/androidLibBuildHelper.groovy
+++ src/com/retailinmotion/androidLibBuildHelper.groovy
@@ -2,19 +2,58 @@ package com.retailinmotion;
 
 /*
 TO DO
-- get webhook setup on one BluetoothLibraryAndroid repo - see example: http://bitbucket.rim.local:7990/plugins/servlet/webhooks/projects/DEVOPS/repos/terraform-eks/details/66
+- get webhook setup on BluetoothLibraryAndroid repo - see example: http://bitbucket.rim.local:7990/plugins/servlet/webhooks/projects/DEVOPS/repos/terraform-eks/details/66
 - confirm library repository with Dev and get Maven setup on Nexus
 - confirm build steps with Dev
 - properly set http://rim-build-05:8080/view/work-in-progress/job/bluetoothlibraryandroid/ to test with
 - https://jenkins.io/doc/book/pipeline/shared-libraries
 - create andriodLibBuildHelper shared lib with function androidLibBuild(params) to have the pipeline steps
 - Jenkinsfile in repo should just import the lib "andriodLibBuildHelper@feature/DEVOPS-316" and call androidLibBuild(params) function defined in the lib
-- investigate solutions for adding tenant customisations - Dev & DevOps
 */
 
 def androidLibBuild(test){
-    println "androidLibBuild Test value: --${test}--"
+/*     println "androidLibBuild Test value: --${test}--"
     def OctopusHelper = new com.retailinmotion.OctopusHelper()
     def jkurl = OctopusHelper.getServer(env.JENKINS_URL)
-    println "Jenkins: --${jkurl}--"
+    println "Jenkins: --${jkurl}--" */
+
+    def buildHelper = new com.retailinmotion.buildHelper()
+    def versionInfo
+
+    pipeline {
+        agent any
+        options { skipDefaultCheckout() }
+        stages {
+            stage('Clean'){
+                steps {
+                    cleanWs()
+                    checkout scm
+                }
+            }
+            stage('Get Version'){
+                steps {
+                    script {
+                        def gitVersionTool=tool name: 'GitVersion-5', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+                        versionInfo=buildHelper.getGitVersionInfo(gitVersionTool)
+                        echo "Version Info:"
+                        echo versionInfo.toString()
+                        
+                        // get the short and long git hashes
+                        gitHashes=buildHelper.getGitHashes()
+                        // Use the informational version for the package name, but replace the full git hash with the short version for display purposes
+                        packageString=versionInfo.SafeInformationalVersion.toString().replace(gitHashes.full, gitHashes.short);
+                        // Update jenkins build name
+                        currentBuild.displayName = "#${versionInfo.FullSemVer}"
+                        currentBuild.description = "${versionInfo.InformationalVersion}"
+                    }
+                }
+            }
+
+            stage('Clean Workspace'){
+                steps {
+                    cleanWs()
+                }
+            }
+        }
+    }
 }
