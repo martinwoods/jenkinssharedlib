3a9c8369eb7817ae70f9a87e7b9088d332fc91b5
Vlad Petrescu <vlad.petrescu@retailinmotion.com>
2020-01-28 09:44:52 +0000

    Test7

--- vars/androidLibBuild.groovy
+++ vars/androidLibBuild.groovy
@@ -1,21 +1,8 @@
-package com.retailinmotion;
-
-/*
-TO DO
-- get webhook setup on BluetoothLibraryAndroid repo - see example: http://bitbucket.rim.local:7990/plugins/servlet/webhooks/projects/DEVOPS/repos/terraform-eks/details/66
-- confirm library repository with Dev and get Maven setup on Nexus
-- confirm build steps with Dev
-- properly set http://rim-build-05:8080/view/work-in-progress/job/bluetoothlibraryandroid/ to test with
-- https://jenkins.io/doc/book/pipeline/shared-libraries
-- create andriodLibBuildHelper shared lib with function androidLibBuild(params) to have the pipeline steps
-- Jenkinsfile in repo should just import the lib "andriodLibBuildHelper@feature/DEVOPS-316" and call androidLibBuild(params) function defined in the lib
-*/
-
-def androidLibBuildSrc(BUILD_NUMBER){
-/*     println "androidLibBuild Test value: --${test}--"
-    def OctopusHelper = new com.retailinmotion.OctopusHelper()
-    def jkurl = OctopusHelper.getServer(env.JENKINS_URL)
-    println "Jenkins: --${jkurl}--" */
+def call (body) {
+	def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
     def buildHelper = new com.retailinmotion.buildHelper()
     def versionInfo
@@ -59,4 +46,5 @@ def androidLibBuildSrc(BUILD_NUMBER){
             }
         }
     }
+
 }
