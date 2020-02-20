72a0ead4ace226276bfa51d51d345400ef15dcd7
Marcelo Cardoso <marcelo.cardoso@retailinmotion.com>
2020-02-20 18:09:54 +0000

    androidLibBuild.groovy edited online with Bitbucket
    renaming gradle task

--- vars/androidLibBuild.groovy
+++ vars/androidLibBuild.groovy
@@ -56,7 +56,7 @@ def call () {
                     }
                     withSonarQubeEnv('SonarQubeServer') {
                         bat './gradlew.bat cleanBuildCache'
-                        bat './gradlew codeCoverageReport'
+                        bat './gradlew jacocoTestReport'
                         bat """./gradlew.bat --info sonarqube assembleRelease ^
                                 -Dsonar.projectKey=${libraryName} -Dsonar.branch.name=${originalBranchName} ^
                                 -Dsonar.projectVersion=${versionInfo.FullSemVer} -Dsonar.projectName=${libraryName}
