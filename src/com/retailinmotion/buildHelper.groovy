58b0321580ae6dc61f87683c7e685aacd8837749
Joseph Sweeney <joseph.sweeney@retailinmotion.com>
2024-08-21 17:20:00 +0100

    DEVOPS-1957

--- src/com/retailinmotion/buildHelper.groovy
+++ src/com/retailinmotion/buildHelper.groovy
@@ -107,7 +107,7 @@ def getGitVersionInfo(dockerImageOrToolPath, dockerContext=null, subPath =null, 
 				elif [ -e /gitversion/gitversion ]; 
 				then
 				    ls -ltr /gitversion
-					/gitversion/gitversion /src${subPath} > gitversion.txt
+					gitversion/gitversion /src${subPath} > gitversion.txt
 				else
 					/usr/bin/dotnet /app/GitVersion.dll /src${subPath} > gitversion.txt
 				fi
