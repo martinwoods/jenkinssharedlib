b96cf378a1d67f37884c38b1815eb6ad3b72d2e3
Joseph Sweeney <joseph.sweeney@retailinmotion.com>
2024-08-22 16:57:20 +0100

    DEVOPS-1957

--- src/com/retailinmotion/buildHelper.groovy
+++ src/com/retailinmotion/buildHelper.groovy
@@ -98,7 +98,7 @@ def getGitVersionInfo(dockerImageOrToolPath, dockerContext=null, subPath =null, 
 		// Execute the command inside the given docker image (intended for use on linux systems)
 		// when using gitversion4, this was done using Mono to call the .exe file
 		// In gitversion5, dotnet core is used instead
-		dockerContext.image(dockerImageOrToolPath).inside("-v \"$WORKSPACE:/src\" -e subPath=\"$subPath\" -e args=\"$args\" -e IGNORE_NORMALISATION_GIT_HEAD_MOVE=1"){
+		dockerContext.image(dockerImageOrToolPath).inside("-v \"$WORKSPACE:/src\" -e subPath=\"$subPath\" -e args=\"-u 0\" -e IGNORE_NORMALISATION_GIT_HEAD_MOVE=1"){
 			sh '''
 				if [ -e /usr/lib/GitVersion/tools/GitVersion.exe ]; 
 				then
@@ -106,10 +106,7 @@ def getGitVersionInfo(dockerImageOrToolPath, dockerContext=null, subPath =null, 
 				elif [ -e /gitversion/gitversion ]; 
 				then
 				    ls -ltr /lib
-					cd /gitversion
-					pwd
-					ls -ltr
-					.gitversion help
+					/gitversion/gitversion help
 				else
 					/usr/bin/dotnet /app/GitVersion.dll /src${subPath} > gitversion.txt
 				fi
